1d7576aa38e39a13540e5ee59f0c09a3f01ce419
KevinRMaier <kevin.r.maier@tum.de>
2023-11-29 17:35:30 +0100

    Added calculation of coefficient of variation and print statement

--- TEM_histogram.py
+++ TEM_histogram.py
@@ -27,7 +27,16 @@ def calculate_d(areas):
     return diameters
 
 
-def plot_hist(diameters):
+def calc_geometry(diameters):
+    sample_size = len(diameters)
+    median_dp = statistics.median(diameters)
+    mean_dp = statistics.mean(diameters)
+    stdev_dp = statistics.stdev(diameters)
+    CV = stdev_dp/mean_dp # coefficient of variation
+    return sample_size, median_dp, mean_dp, stdev_dp, CV
+
+
+def plot_hist(diameters, sample_size, median_dp, stdev_dp):
     """if other particles, than some between 0 and 40 nm are measured, change bins = np.arange to (lower size lim,
     upper size lim, width of bins in nm) and in ax.st(..., xlim(lower size lim, upper size lim) according to the
     measured sizes - this could be automated"""
@@ -36,9 +45,6 @@ def plot_hist(diameters):
     fig, ax = plt.subplots(figsize=(18.5 * cm, 10 * cm))  # height with title 12, without 10
     ax.hist(diameters, bins=bins, density=True)
     ax.set(xlabel='particle diameter / nm', ylabel='prevalence of particles with given diameter', xlim=(0, 40))
-    sample_size = len(diameters)
-    median_dp = statistics.median(diameters)
-    stdev_dp = statistics.stdev(diameters)
     plt.title(f"n = {sample_size}, median = {round(median_dp, 2)} \u00B1 {round(stdev_dp, 2)} nm")
     plt.show()
     return ax
@@ -48,5 +54,6 @@ if __name__ == "__main__":
 
     areas = import_data()
     diameters = calculate_d(areas)
-    ax = plot_hist(diameters)
-
+    sample_size, median_dp, mean_dp, stdev_dp, CV = calc_geometry(diameters)
+    ax = plot_hist(diameters, sample_size, median_dp, stdev_dp)
+    print(f"median = {median_dp}, mean = {mean_dp}, sigma = {stdev_dp}, CV = {CV}, sample size = {sample_size}")
